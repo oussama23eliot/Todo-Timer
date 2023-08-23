e4776d19b4769794f7a3a83f8526869ac9090763
Oussama Bentoufile <Oussa2ben7@gmail.com>
2023-08-24 00:56:35 +0100

    url bug fixed

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ import { RouterProvider, BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    // <PubliLayout>
       <BrowserRouter>
         <Routes>
           <Route element={<PubliLayout />}>
-            <Route path='todo' element={<Todo />} />
-            <Route path='timer' element={<Timer />} />
+            <Route path='Todo-Timer/todo' element={<Todo />} />
+            <Route path='Todo-Timer/timer' element={<Timer />} />
           </Route>
         </Routes>
       </BrowserRouter>
-    // </PubliLayout>
   );
 }
 
