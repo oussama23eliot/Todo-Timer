5d43596e2f69f010f81b0ae53711e05ccb2e8f8c
Oussama Bentoufile <Oussa2ben7@gmail.com>
2023-08-24 01:00:43 +0100

    url bug fixed 2

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<PubliLayout />}>
-            <Route path='Todo-Timer/todo' element={<Todo />} />
-            <Route path='Todo-Timer/timer' element={<Timer />} />
+            <Route path='/' element={<Todo />} />
+            <Route path='todo' element={<Todo />} />
+            <Route path='timer' element={<Timer />} />
           </Route>
         </Routes>
       </BrowserRouter>
