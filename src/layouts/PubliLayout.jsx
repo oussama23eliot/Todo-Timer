130ea395fc19161ef3ccb588179be2eeb1580579
Oussama Bentoufile <Oussa2ben7@gmail.com>
2023-08-24 02:20:07 +0100

    url bug fixed 11

--- src/layouts/PubliLayout.jsx
+++ src/layouts/PubliLayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import { Outlet } from 'react-router-dom'
+import Todo from '../pages/Todo'
 
 
 export default function PubliLayout({ children, ...attr }) {
     return (
         <div className='scroll-smooth pb-20'>
             <Navbar />
-            <Outlet/>
+            <Todo/>
         </div>
 
     )
