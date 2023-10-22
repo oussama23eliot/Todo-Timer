4c9b884c58bd77fd729ad640a1225a115485e103
Oussama Bentoufile <Oussa2ben7@gmail.com>
2023-10-22 22:39:58 +0100

    favicon added for vercel deployment

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route  element={<PubliLayout />}>
+            <title>TodoTimer </title>
             <Route path='/' element={<Todo />} />
             <Route path='/todo' element={<Todo />} />
             <Route path='/timer' element={<Timer />} />
